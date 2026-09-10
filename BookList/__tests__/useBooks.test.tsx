e3b44201561691de32ac4230dfc315fd3fa431d7
Eldaram <dragan.ribes@gmail.com>
2026-09-10 14:53:15 +0200

    fix: A small merge issue fixed

--- BookList/__tests__/useBooks.test.tsx
+++ BookList/__tests__/useBooks.test.tsx
@@ -1,7 +1,7 @@
 import { booksList } from "../features/books/booksList";
-import { bookRepository } from "../services/repository/bookRepository";
+import { bookRepository } from "../services/servicesImpl/bookServiceImpl";
 
-jest.mock("../services/repository/bookRepository");
+jest.mock("../services/servicesImpl/bookServiceImpl");
 
 describe("Books Pagination Feature Suite", () => {
   beforeEach(() => {
